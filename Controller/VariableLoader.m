3e67f06cce81a9455e35cef80f5b6dea02dc4436
DrGrandmaster <46546882+DrGrandmaster@users.noreply.github.com>
2025-10-09 00:13:37 -0600

    Stuff

--- Controller/VariableLoader.m
+++ Controller/VariableLoader.m
@@ -1,5 +1,7 @@
 %% Loads important variables into your workspace.
 
+% 
+
 %% Constants
 
 % Gravitational acc'n, m/s^2
@@ -8,5 +10,5 @@ g = 9.80665;
 % Length of Rope, m
 l = 3.7;
 
-% Linear Plant Model
+%% Linear Plant Model
 G = tf([g], [l 0 g 0]);
